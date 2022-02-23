5c8735d2f5bc0eeb3e3e7a53bcd2d55d8b1e69f3
Rene Hernandez <rene83120@gmail.com>
2022-02-23 10:13:38 -0800

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = document.querySelector("input#searchByID");
+
+    fetch("http://localhost:3000/movies/${iput.value}")
+      .then((response) => response.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
